42f9125f051cad56e76eee7502c5fc6e85982317
NUM_24 <1018810506@qq.com>
2018-10-19 18:36:19 +0800

    add parameters

--- PLSA/vision/lib.py
+++ PLSA/vision/lib.py
@@ -5,17 +5,19 @@ The function of this Module is served for visualizing common curve.
 
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-def plot_cphCoef(df, coef_col='coef', se_col='se(coef)', c_col='p', name_col=None, ci=0.95,
-                 xlabel="Name of variable", ylabel="Coefficient", title="Variable's coefficient of CPH model", 
+def plot_cphCoef(dfx, coef_col='coef', se_col='se(coef)', c_col='p', name_col=None, ci=0.95,
+                 error_bar='hr', xlabel="Name of variable", ylabel="", 
+                 title="Variable's coefficient of CPH model", 
                  figsize=(8, 6), save_fig_as=""):
     """Visualize variables' coefficient in lifelines.CPH model
 
     Parameters
     ----------
-    df : pandas.DataFrame
+    dfx : pandas.DataFrame
         Object equals to cph.summary.
     coef_col : str
         Name of column indicating coefficient.
@@ -27,6 +29,9 @@ def plot_cphCoef(df, coef_col='coef', se_col='se(coef)', c_col='p', name_col=Non
         Name of x-axis's column. 
     ci : float
         Confidence interval, default 0.95.
+    error_bar : str
+        Type of error bars, 'hr' for asymmetrical error bars,
+        'log-hr' for symmetrical error bars.
 
     Returns
     -------
@@ -37,20 +42,39 @@ def plot_cphCoef(df, coef_col='coef', se_col='se(coef)', c_col='p', name_col=Non
     --------
     >>> plot_cphCoef(cph.summary, 'coef', 'se(coef)', 'p')
     """
+    df = dfx.copy(deep=True)
     N = len(df)
     if name_col is None:
         name_col = 'name__'
         df[name_col] = df.index
-    df['idx__'] = range(N)
+    df['idx'] = range(N)
     df['1 - P-value'] = 1 - df[c_col]
-    # Calculate p for CI
-    df['CI__'] = abs(norm.ppf((1-ci)/2)) * df[se_col]
+    # Calculate CI
+    df['CI'] = abs(norm.ppf((1-ci)/2)) * df[se_col]
     # Plot figure
     fig, ax = plt.subplots(figsize=figsize)
-    df.plot.scatter(x='idx__', y=coef_col, c='1 - P-value', 
+    if error_bar == 'log-hr':
+        df.plot.scatter(x='idx', y=coef_col, c='1 - P-value', 
                     marker='s', s=120, cmap=plt.cm.get_cmap('YlOrRd'), ax=ax)
-    ax.axhline(y=0, linestyle='--', color='black', linewidth=0.5)
-    df.plot(x='idx__', y=coef_col, kind='bar', yerr='CI__', color="None", legend=False, ax=ax)
+        ylabel = ('Coefficient' if ylabel == '' else ylabel)
+        ax.axhline(y=0, linestyle='--', color='black', linewidth=1)
+        ax.errorbar(df['idx'], df[coef_col], yerr=df['CI'],
+                    ecolor='black', elinewidth=0.8, linestyle='')
+    elif error_bar == 'hr':
+        # calculate
+        df['er_lower'] = np.exp(df[coef_col]) - np.exp(df[coef_col] - df['CI'])
+        df['er_upper'] = np.exp(df[coef_col] + df['CI']) - np.exp(df[coef_col])
+        df[coef_col] = np.exp(df[coef_col])
+        df.plot.scatter(x='idx', y=coef_col, c='1 - P-value',
+                    marker='s', s=120, cmap=plt.cm.get_cmap('YlOrRd'), ax=ax)
+        ylabel = ('Hazard Ratio' if ylabel == '' else ylabel)
+        ax.axhline(y=1, linestyle='--', color='black', linewidth=1)
+        ax.errorbar(df['idx'], df[coef_col], yerr=[df['er_lower'].values, df['er_upper'].values],
+                    ecolor='black', elinewidth=0.8, linestyle='')
+    else:
+        # TODO
+        pass
+    ax.set_xticks(list(df['idx']))
     ax.set_xticklabels(list(df[name_col]), rotation=0)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -58,5 +82,5 @@ def plot_cphCoef(df, coef_col='coef', se_col='se(coef)', c_col='p', name_col=Non
     plt.show()
     if save_fig_as != "":
         fig.savefig(save_fig_as, format='png', dpi=600)
-    # Drop columns
-    df.drop(['name__', 'idx__', '1 - P-value', 'CI__'], axis=1, inplace=True, errors='ignore')
+    # Drop DataFrame
+    del df
